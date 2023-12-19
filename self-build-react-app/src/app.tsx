a971825fbc90416734a7a524ee21574f665c8d19
chencs <chunsheng.chen@coolcollege.cn>
2023-12-19 17:38:38 +0800

    feat: css/less 模块化配置

--- self-build-react-app/src/app.tsx
+++ self-build-react-app/src/app.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import './app.css';
-import './app.less'
+// import styles from './app.css';
+import styles from './app.less'
 
 const App = () => {
   console.log("NODE_ENV", process.env.NODE_ENV);
   console.log("BASE_ENV", process.env.BASE_ENV);
 
-  return <div>chencs</div>
+  return <div className={styles.cool}>chencs</div>
 }
 
 export default App;
